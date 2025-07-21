2c9d4cbee73ec2d2026e4a1fc63d0887dcab5cb5
Ritu Arora <jsr.ritu.jsr@gmail.com>
2025-07-21 17:35:51 -0400

    message fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
             <h1 class="display-4" id="main-heading">{{ __('home.name') }}</h1>
             <h2 class="tagline">{{ __('home.tagline') }}</h2>
             <p class="lead">{{ __('home.intro') }}</p>
-            <p class="mt-3">{!! __('home.message') !!}</p>
+            <!-- <p class="mt-3">{!! __('home.message') !!}</p> -->
             <a class="btn btn-primary btn-lg"
                 href="{{ asset(app()->getLocale() === 'fr' ? 'docs/resume_fr.pdf' : 'docs/resume_en.pdf')}}"
                 role="button" target="_blank" rel="noopener noreferrer">
@@ -20,8 +20,15 @@
             <!-- </div>
     <div class="profile-image">
       <img src="{{ asset('images/dp-nobg2.png') }}" alt="{{ __('home.profile_alt') }}" class="img-fluid" />
-    </div>
-    </div> -->
+     </div> -->
+        </div>
 </section>
+<!-- New message section below jumbotron -->
+<section class="py-4 bg-dark text-light text-center">
+    <div class="container">
+        <p class="lead mb-0">{!! __('home.message') !!}</p>
+    </div>
+</section>
+
 
 @endsection
