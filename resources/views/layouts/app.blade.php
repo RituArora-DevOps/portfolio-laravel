0cdba2f33d96adcc8b32fdf9c69c1c6910bcbea1
Ritu Arora <jsr.ritu.jsr@gmail.com>
2025-07-21 13:40:23 -0400

    vite in action

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,16 @@
     <title>@yield('title', 'Dr. Ritu Arora | Portfolio')</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/style-modular.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/style-modular.css') }}"> -->
+    <!-- The above line is redundant as Vite is handling SCSS compilation-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     @stack('styles')
+    {{-- Vite assets --}}
+    @vite(['resources/scss/style-modular.scss', 'resources/js/app.js'])
 </head>
 
 <body>
